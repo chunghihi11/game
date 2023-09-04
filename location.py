73683b30d5567d234a5a6b115fd728c5c8658f67
chunghihi11 <101981189+chunghihi11@users.noreply.github.com>
2023-09-04 17:57:53 +0530

    Add files via upload

---
+++ location.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+while True:
+    # Get the current cursor position
+    cursor_position = pyautogui.position()
+    
+    # Print the cursor position
+    print("Cursor position:", cursor_position)
+    
---
+++ test.py
@@ -0,0 +1,5 @@
+def callfun():
+    print('hii')
+    pass
+while True:
+    callfun()
